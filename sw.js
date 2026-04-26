89b26cbbcf0fe93665e4f02e7b85ac62ffb9eb68
WooAraBin <dogeunk@gmail.com>
2026-04-27 01:30:16 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'lenz-v7';
+const CACHE = 'lenz-v8';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
